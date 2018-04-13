c226b0f8fa869683ccd99f81af4de74efa586b5e
Eddy Beaupré <eddy@beaupre.biz>
2018-04-12 21:26:40 -0400

    French Language Translation.

---
+++ app/System/Language/fr/Welcome.php
@@ -0,0 +1,61 @@
+<?php
+/**
+* Language File
+* Home
+* French Language
+*
+* UserApplePie
+* @author David (DaVaR) Sargent <davar@userapplepie.com>
+* @translator Eddy Beaupré <eddy@beaupre.biz>
+* @version 4.0.1
+*/
+
+return [
+	// Home method
+	'homeText' => 'Home',
+	'homeMessage' => '
+		Bonjour, Bienvenue depuis le contrôleur Home! <br/>
+		Ce contenu peut être modifié dans <code>/app/Views/Home/Home.php</code>
+	',
+
+	// Contact method
+	'contactText' => 'Contact',
+	'contactMessage' => '
+		Bonjour, Bienvenue depuis le contrôleur Home et la méthode contact! <br/>
+		Ce contenu peut être modifié dans <code>/app/Views/Home/Contact.php</code>
+	',
+
+	// About method
+	'aboutText' => 'About',
+	'aboutMessage' => '
+		Bonjour, Bienvenue depuis le contrôleur Home et la méthode About! <br/>
+		Ce contenu peut être modifié dans <code>/app/Views/Home/About.php</code>
+	',
+
+	// Buttons
+	'openHome' => 'Accueil',
+	'openAbout' => 'À propos',
+	'openContact' => 'Contact',
+
+	// Global
+	'uap_home' => "Accueil",
+	'uap_about' => "À propos",
+	'uap_contact' => "Contact",
+	'uap_logout' => "Déconnexion",
+	'uap_forum' => "Forum",
+	'uap_view_profile' => "Voir le profil",
+	'uap_account_settings' => "Paramètres du compte",
+	'uap_admin_panel' => "Panneau d'administration",
+	'uap_private_messages' => "Messages privés",
+	'uap_success' => "Succès",
+	'uap_error' => "Erreur",
+	'uap_pages_displaying' => "Affichage",
+	'uap_friends' => "Amis",
+
+	// Lang Change Success Message
+	'lang_change_success' => "Vous avez changé la langue du site.",
+
+	// Footer
+	'uap_poweredby' => "Propulsé par",
+	'uap_all_rights' => "Tous droits réservés",
+];
